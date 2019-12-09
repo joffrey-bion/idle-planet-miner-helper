9915c1733aada3116b93cb53fbbe9dcc4b960783
Joffrey Bion <joffrey.bion@booking.com>
2019-12-09 09:58:57 +0100

    Remove beacon ranges using planets that are not declared yet

--- src/main/kotlin/org/hildan/ipm/helper/Beacon.kt
+++ src/main/kotlin/org/hildan/ipm/helper/Beacon.kt
@@ -6,9 +6,9 @@ private val planets = PlanetType.values()
 
 enum class BeaconPlanetRange(val planets: List<PlanetType>) {
     RANGE_1_4(planets.slice(0..3)),
-//    RANGE_5_7(planets.slice(4..6)),
-//    RANGE_8_10(planets.slice(7..9)),
-//    RANGE_11_13(planets.slice(10..13))
+    //    RANGE_5_7(planets.slice(4..6)),
+    //    RANGE_8_10(planets.slice(7..9)),
+    //    RANGE_11_13(planets.slice(10..13))
     // TODO complete ranges when PlanetType is complete
 }
 
