9fc4e1b01d92fb30538194cbff1d8b01cb953a82
Joffrey Bion <joffrey.bion@booking.com>
2019-12-11 23:40:37 +0100

    Improve price formatting

--- src/main/kotlin/org/hildan/ipm/helper/galaxy/Money.kt
+++ src/main/kotlin/org/hildan/ipm/helper/galaxy/Money.kt
@@ -5,4 +5,13 @@ inline class Price(val amount: Double) {
     constructor(amount: Int) : this(amount.toDouble())
 
     operator fun times(factor: Double) = Price(amount * factor)
+
+    override fun toString(): String = when {
+        amount < 1_000 -> format(amount, "")
+        amount < 1_000_000 -> format(amount / 1_000, "k")
+        amount < 1_000_000_000 -> format(amount / 1_000_000, "M")
+        else -> format(amount / 1_000_000_000, "B")
+    }
+
+    private fun format(x: Double, unit: String): String = String.format("\$%.2f$unit", x)
 }
