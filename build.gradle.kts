993dd786f792dec54c8514c53b26602015892621
Joffrey Bion <joffrey.bion@booking.com>
2019-12-23 00:49:57 +0100

    Add coroutines dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.3")
 
     testImplementation(kotlin("test-junit"))
     testImplementation("junit:junit:4.12")
