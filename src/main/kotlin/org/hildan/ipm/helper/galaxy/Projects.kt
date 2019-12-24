3b0b96eeefb5838d7bc16febe487659a4e5d653f
Joffrey Bion <joffrey.bion@booking.com>
2019-12-24 03:30:00 +0100

    Add a bunch more projects

--- src/main/kotlin/org/hildan/ipm/helper/galaxy/Projects.kt
+++ src/main/kotlin/org/hildan/ipm/helper/galaxy/Projects.kt
@@ -45,18 +45,25 @@ enum class Project(
     ADVANCED_MINING(Resources.of(5 of BATTERY, 20 of ALUMINUM_BAR), Bonus.allPlanets(mineRate = 1.25)),
     ADVANCED_THRUSTERS(Resources.of(2 of GLASS, 10 of GOLD_BAR), Bonus.allPlanets(shipSpeed = 1.25)),
     ADVANCED_CARGO_HANDLING(Resources.of(5 of HAMMER, 25 of SILVER_BAR), Bonus.allPlanets(cargo = 1.25)),
+    SUPERIOR_MINING(Resources.of(10 of LASER_TORCH, 25 of PLATINUM_BAR), Bonus.allPlanets(mineRate = 1.25)),
+    SUPERIOR_THRUSTERS(Resources.of(4 of ADVANCED_BATTERY), Bonus.allPlanets(shipSpeed = 1.25)),
+    SUPERIOR_CARGO_HANDLING(Resources.of(50 of TITANIUM_BAR), Bonus.allPlanets(cargo = 1.25)),
 
-    COLONY_TAX_INCENTIVES(Resources.of(60 of ALUMINUM_BAR),
-        Bonus(planetUpgradeCost5pReductions = 1)
-    ),
-    COLONY_ADVANCED_TAX_INCENTIVES(Resources.of(60 of BRONZE_BAR),
-        Bonus(planetUpgradeCost5pReductions = 1)
-    ),
+    COLONIZATION(Resources.of(20 of COPPER_BAR, 10 of IRON_BAR)),
+    COLONIZATION_SCOUTING(Resources.of(15 of IRON_NAIL)),
+    COLONY_TAX_INCENTIVES(Resources.of(60 of ALUMINUM_BAR), Bonus(planetUpgradeCost5pReductions = 1)),
+    COLONY_ADVANCED_TAX_INCENTIVES(Resources.of(60 of BRONZE_BAR), Bonus(planetUpgradeCost5pReductions = 1)),
+    COLONY_SUPERIOR_TAX_INCENTIVES(Resources.of(60 of PALADIUM_BAR), Bonus(planetUpgradeCost5pReductions = 1)),
 
     ADVANCED_FURNACE(Resources.of(3 of GLASS, 10 of ALUMINUM_BAR), Bonus.production(smeltSpeed = 1.2)),
     ADVANCED_CRAFTER(Resources.of(5 of LENSE, 50 of GOLD_BAR), Bonus.production(craftSpeed = 1.2)),
-//    SMELTING_EFFICIENCY(Resources.of(200 of BRONZE), Bonus.production(smeltIngredients = 0.8))
-//    CRAFTING_EFFICIENCY(Resources.of(40 of SOLAR_PANEL), Bonus.production(craftIngredients = 0.8))
+    ADVANCED_ALLOY_VALUE(Resources.of(3 of CIRCUIT, 10 of SILVER_BAR), Bonus.values(alloysMultiplier = 1.2)),
+    //    SUPERIOR_ALLOY_VALUE(Resources.of(...), Bonus.values(alloysMultiplier = 1.2)),
+    ADVANCED_ITEM_VALUE(Resources.of(1 of LENSE, 5 of SILVER_BAR), Bonus.values(itemsMultiplier = 1.2)),
+    SMELTING_EFFICIENCY(Resources.of(200 of BRONZE_BAR), Bonus.production(smeltIngredients = 0.8)),
+    CRAFTING_EFFICIENCY(Resources.of(40 of SOLAR_PANEL), Bonus.production(craftIngredients = 0.8)),
+    SUPERIOR_FURNACE(Resources.of(5 of IRIDIUM_BAR, 20 of TITANIUM_BAR, 50 of PLATINUM_BAR), Bonus.production(smeltSpeed = 1.2)),
+    SUPERIOR_CRAFTING(Resources.of(2 of THERMAL_SCANNER, 10 of ADVANCED_BATTERY, 20 of LASER_TORCH), Bonus.production(craftSpeed = 1.2)),
     // TODO fill in all projects
 }
 
@@ -66,14 +73,20 @@ private object ProjectGraph {
         Project.ASTEROID_MINER to setOf(Project.SMELTER, Project.ROVER),
         Project.ROVER to setOf(Project.ADVANCED_MINING),
         Project.ADVANCED_MINING to setOf(Project.ADVANCED_THRUSTERS, Project.ADVANCED_CARGO_HANDLING),
-        Project.ADVANCED_THRUSTERS to setOf(),// Project.SUPERIOR_MINING),
-        Project.ADVANCED_CARGO_HANDLING to setOf(),// Project.SUPERIOR_MINING),
+        Project.ADVANCED_THRUSTERS to setOf(Project.SUPERIOR_MINING),
+        Project.ADVANCED_CARGO_HANDLING to setOf(Project.SUPERIOR_MINING),
+        Project.SUPERIOR_MINING to setOf(Project.SUPERIOR_THRUSTERS, Project.SUPERIOR_CARGO_HANDLING),
 
         Project.SMELTER to setOf(Project.CRAFTER, Project.ADVANCED_FURNACE),
         Project.CRAFTER to setOf(Project.ADVANCED_CRAFTER),
-        Project.ADVANCED_FURNACE to setOf(), // SMELTING_EFFICIENCY, ADVANCED_ALLOY_VALUE
+        Project.ADVANCED_FURNACE to setOf(Project.SMELTING_EFFICIENCY, Project.ADVANCED_ALLOY_VALUE),
+        Project.ADVANCED_CRAFTER to setOf(Project.CRAFTING_EFFICIENCY, Project.ADVANCED_ITEM_VALUE),
+        Project.SMELTING_EFFICIENCY to setOf(Project.SUPERIOR_FURNACE),
+        Project.CRAFTING_EFFICIENCY to setOf(Project.SUPERIOR_CRAFTING),
+        Project.SUPERIOR_FURNACE to setOf(), // PREFERRED_VENDOR, FURNACE_OVERDRIVE
+        Project.SUPERIOR_CRAFTING to setOf(),
 
-        Project.MANAGEMENT to setOf(Project.TELESCOPE_1),// Project.COLONIZATION),
+        Project.MANAGEMENT to setOf(Project.TELESCOPE_1, Project.COLONIZATION),
         Project.TELESCOPE_1 to setOf(Project.BEACON, Project.TELESCOPE_2),
         Project.BEACON to setOf(),
         Project.TELESCOPE_2 to setOf(Project.TELESCOPE_3), // + resource details
@@ -81,7 +94,13 @@ private object ProjectGraph {
         Project.TELESCOPE_4 to setOf(Project.TELESCOPE_5, Project.CARGO_LOGISTICS),
         Project.CARGO_LOGISTICS to setOf(Project.ORE_TARGETING),
         Project.TELESCOPE_5 to setOf(Project.TELESCOPE_6),
-        Project.TELESCOPE_6 to setOf(Project.TELESCOPE_7)
+        Project.TELESCOPE_6 to setOf(Project.TELESCOPE_7),
+
+        Project.COLONIZATION to setOf(Project.COLONIZATION_SCOUTING), // COLONIZATION_EFFICIENCY
+        Project.COLONIZATION_SCOUTING to setOf(Project.COLONY_TAX_INCENTIVES),
+        Project.COLONY_TAX_INCENTIVES to setOf(Project.COLONY_ADVANCED_TAX_INCENTIVES),
+        Project.COLONY_ADVANCED_TAX_INCENTIVES to setOf(Project.COLONY_SUPERIOR_TAX_INCENTIVES)
+
         // TODO fill in other project dependencies
     )
 }
