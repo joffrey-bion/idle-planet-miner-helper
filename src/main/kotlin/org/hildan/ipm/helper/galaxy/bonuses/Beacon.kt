64f0925f8e72dbdbbb89eb185fef4e0626ce3183
Joffrey Bion <joffrey.bion@booking.com>
2019-12-25 02:29:13 +0100

    Add more beacon ranges

--- src/main/kotlin/org/hildan/ipm/helper/galaxy/bonuses/Beacon.kt
+++ src/main/kotlin/org/hildan/ipm/helper/galaxy/bonuses/Beacon.kt
@@ -3,14 +3,22 @@ package org.hildan.ipm.helper.galaxy.bonuses
 import org.hildan.ipm.helper.galaxy.planets.PlanetType
 import org.hildan.ipm.helper.utils.completedBy
 
-private val planets = PlanetType.values()
+private val allPlanets = PlanetType.values()
 
 enum class BeaconPlanetRange(val planets: List<PlanetType>) {
-    RANGE_1_4(planets.slice(0..3)),
-    RANGE_5_7(planets.slice(4..6)),
-    RANGE_8_10(planets.slice(7..9)),
-    //    RANGE_11_13(planets.slice(10..13))
+    RANGE_1_4(1, 4),
+    RANGE_5_7(5, 7),
+    RANGE_8_10(8, 10),
+    RANGE_11_13(11, 13),
+    RANGE_14_16(14, 16),
+    RANGE_17_19(17, 19),
+    RANGE_20_22(20, 22),
+    RANGE_23_25(23, 25),
+    RANGE_26_28(26, 28),
+    RANGE_29_31(29, 31);
     // TODO complete ranges when PlanetType is complete
+
+    constructor(from: Int, to: Int): this(allPlanets.slice(from..to))
 }
 
 object Beacon {
