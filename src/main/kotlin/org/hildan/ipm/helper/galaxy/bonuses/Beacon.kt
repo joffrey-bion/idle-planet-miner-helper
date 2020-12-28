1d69b0426d3973b823f7c7a5218c391741380942
Joffrey Bion <joffrey.bion@gmail.com>
2020-12-28 17:18:11 +0100

    Code style in Beacon.kt

--- src/main/kotlin/org/hildan/ipm/helper/galaxy/bonuses/Beacon.kt
+++ src/main/kotlin/org/hildan/ipm/helper/galaxy/bonuses/Beacon.kt
@@ -5,10 +5,9 @@ import org.hildan.ipm.helper.galaxy.planets.Planet
 data class BeaconRangeBonus(
     val from: Int,
     val to: Int,
-    val bonus: PlanetBonus
+    val bonus: PlanetBonus,
 )
 
 private val BeaconRangeBonus.planets: List<Planet> get() = Planet.values().slice((from - 1) until to)
 
-fun List<BeaconRangeBonus>.asSingleBonus() =
-        Bonus.perPlanet(flatMap { it.planets.map { p -> p to it.bonus } }.toMap())
+fun List<BeaconRangeBonus>.asSingleBonus() = Bonus.perPlanet(flatMap { it.planets.map { p -> p to it.bonus } }.toMap())
